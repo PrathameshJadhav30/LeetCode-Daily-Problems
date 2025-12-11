1c781a650dc8ca9b6812f3e586d46196fd3d6fde
Prathamesh <prathameshaj2004@gmail.com>
2025-12-11 22:14:17 +0530

    LeetCode Daily

---
+++ 103-Count Covered Buildings/CountCoveredBuildings.cpp
@@ -0,0 +1,58 @@
+// Problem Link: https://leetcode.com/problems/count-covered-buildings/description
+// Date: 11/12/2025
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        vector<int> minx(n + 1, n + 1);
+        vector<int> maxx(n + 1, 0);
+        vector<int> miny(n + 1, n + 1);
+        vector<int> maxy(n + 1, 0);
+
+        for (auto b : buildings) {
+            int x = b[0];
+            int y = b[1];
+
+            minx[y] = min(minx[y], x);
+            maxx[y] = max(maxx[y], x);
+
+            miny[x] = min(miny[x], y);
+            maxy[x] = max(maxy[x], y);
+        }
+
+        int ans = 0;
+        for (auto b : buildings) {
+            int x = b[0];
+            int y = b[1];
+
+            if (minx[y] < x && x < maxx[y] &&
+                miny[x] < y && y < maxy[x]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+
+    int n = 5;
+    vector<vector<int>> buildings = {
+        {1, 2},
+        {3, 2},
+        {1, 4},
+        {3, 4}
+    };
+
+
+    Solution sol;
+    int result = sol.countCoveredBuildings(n, buildings);
+
+    cout << "Covered Buildings: " << result << endl;
+
+    return 0;
+}
